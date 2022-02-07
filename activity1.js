9522d7f18b5de2b5b7050f7b2d23727259057656
prakrambehal <84965705+prakrambehal@users.noreply.github.com>
2022-02-07 20:57:28 +0530

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,11 @@
+score=0;
+function update () {
+    score=score+1;
+    document.getElementById("score").innerHTML=score;
+}
+function save(){
+    localStorage.setItem("score",score)
+}
+function next(){
+    window.location="activity_2.html"
+}
